826094093eb0082b6344b71ab47eaeb6d16ccb46
shu <satos19860124@gmail.com>
2021-06-30 19:43:48 +0900

    最初のコミット

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+// -----------------------------------------------------
+// Plugin
+// -----------------------------------------------------
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sassGlob = require('gulp-sass-glob')
+const autoprefixer = require('gulp-autoprefixer');
+const { watch } = require('gulp');//4系はこれが必要
+
+// -----------------------------------------------------
+// 手順
+// -----------------------------------------------------
+// sassファイルの各ファイルをstyle.scssへ一括import
+// style.scssをcssファイルへコンパイル
+// コンパイルしたcssファイルをindex.htmlで読み込み
+
+/* sass */
+// 実行コマンド：gulp sass
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')//コンパイル前:scssフォルダ直下
+    .pipe(sassGlob())//追加
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({ outputStyle: 'expanded' }))//コンパイル後の見た目を綺麗にする
+    .pipe(gulp.dest('./css/'));//コンパイル後
+});
+
+/* prefix */
+// 実行コマンド：gulp prefix
+gulp.task("prefix", function () {
+  return gulp.src('./css/*.css')//変更前のディレクトリはcss直下フォルダ全て
+    .pipe(autoprefixer({
+      // IE11以上、Androidは5以上
+      "overrideBrowserslist": ["last 2 versions", "ie >= 11", "Android >= 5"],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./dist/'))//distディレクトリに吐き出し
+});
+
+// -----------------------------------------------------
+// watchで監視
+// -----------------------------------------------------
+gulp.task('watch', () => {
+  watch('./scss/**/*.scss', gulp.series('sass')),//ccssフォルダに変更があったらタスクの実行
+    watch('./css/**/*.css', gulp.series('prefix'));
+});
---
+++ js/app.js
@@ -0,0 +1,103 @@
+/*
+js処理をこちらに記述
+**/
+$(function () {
+
+  /* 1.headerのパララックスデザイン*/
+  // =========================================
+  // 処理の流れ
+  // =========================================
+  // windowsをスクロールしたらイベントセット
+  // heroバナーの高さを取得
+  //　windowのスクロール量を取得し、heroバナーの高さを超えるかどうか判定
+  // 超えたらis-activeをつける
+
+
+  // windowsをスクロールしたらイベントセット
+  $(window).on('scroll', function () {
+
+    // heroバナーの高さを取得
+    var heroHeight = $('.js-hero').height();
+    //　windowのスクロール量を取得し、heroバナーの高さを超えるかどうか判定
+    // 超えたらis-activeをつけ
+    $('.js-float-menu').toggleClass("is-active", $(this).scrollTop() > heroHeight);
+
+  });
+
+
+  /* 2.ハンバーガーメニュー*/
+  // =========================================
+  // 処理の流れ
+  // =========================================
+  // 「三」をクリックしたらイベントセット
+  // is-activeをつける・外すを処理
+  // 再度クリックしたらis-activeクラスを外す
+  $('.js-hamburgerMenu').on('click', function () {
+
+    // 「三」へのアニメーション
+    $(this).toggleClass('is-active');
+    // navをspメニューで表示
+    $('.js-spMenu').toggleClass('is-active');
+
+  });
+
+  /* 3.ハンバーガーメニューを閉じる*/
+  // =========================================
+  // 処理の流れ
+  // =========================================
+  // （前提）ハンバーガーメニューが表示されている状態
+  // メニューのどれかをクリックしたら、イベントセット
+  // L 「×」になっているメニューを閉じる（is-activeクラスを外す）
+  // L 開いているハンバーガーメニューを閉じる（is-activeクラスを外す）
+
+  $('.js-close-spMenu').on('click', function () {
+
+    // 「×」を「三」に戻す
+    $('.js-hamburgerMenu').removeClass('is-active');
+    // spメニューを左に戻す
+    $('.js-spMenu').removeClass('is-active');
+
+
+
+  });
+
+});
+
+/* 4.スクロールしたらふわっと表示されるボタン*/
+// =========================================
+// 処理の流れ
+// =========================================
+// 「三」をクリックしたらイベントセット
+// is-activeをつける・外すを処理
+// 再度クリックしたらis-activeクラスを外す
+$(document).ready(function () {
+  $(".js-click-topBtn").hide();　//ボタンを非表示にする
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 100) { //ページの上から100pxスクロールした時
+      $(".js-click-topBtn").fadeIn("fast"); //ボタンがフェードインする
+    } else {
+      $(".js-click-topBtn").fadeOut("fast");　//ボタンがフェードアウトする
+    }
+    scrollHeight = $(document).height(); //ドキュメントの高さ
+    scrollPosition = $(window).height() + $(window).scrollTop(); //現在地
+    footHeight = $("footer").innerHeight(); //止めたい位置の高さ(今回はfooter)
+    if (scrollHeight - scrollPosition <= footHeight) { //ドキュメントの高さと現在地の差がfooterの高さ以下の時
+      $(".js-click-topBtn").css({
+        "position": "absolute", //pisitionをabsoluteに変更
+      });
+    } else { //それ以外の場合は
+      $(".js-click-topBtn").css({
+        "position": "fixed", //固定表示
+      });
+    }
+  });
+
+
+  //スムーススクロール設定
+  $('.js-click-topBtn').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);　//スムーススクロールの速度
+    return false;
+  });
+});
